4f5164a61bd3522d03825530be0d3c61e6cd097d
Daria Korolenko <dariakorolenko@MacBook-Air-Daria.local>
2026-02-13 13:48:41 +0200

    module2 practice

---
+++ index.js
@@ -0,0 +1,87 @@
+// let numb = prompt("Please write the number")
+// let deg = prompt ("Please write the degree")
+// console.log(Number(numb) ** Number(deg))
+
+// const min = 10;
+// const max = 50;
+// let result = Math.round(Math.random() * (max - min) + min) ;
+// console.log(result);
+
+// const colors = ["orange", "red", "blue", "pink", "yellow", "black", "purple"];
+// const min = 0;
+// const max = colors.length - 1;
+// let index = Math.round(Math.random() * (max - min) + min);
+// console.log(colors[index]);
+
+// let firstName = prompt("Enter your first name");
+// let lastName = prompt ("Enter your last name");
+// let room = prompt("Enter your number room");
+// let type = prompt("Enter your type room ");
+
+// let welcomeMsg = `Hello ${firstName} ${lastName }! Welcome to your ${type} ${room}`;
+
+// console.log(welcomeMsg)
+
+
+// function checkForSpam(message) {
+//   let result;
+//   // Change code below this line
+// let changeMessage = message.toLowerCase();
+//   console.log(changeMessage);
+//   if (changeMessage.includes('spam') || changeMessage.includes('sale')){   
+//     result = 'true';
+//   }else {result = 'false';}
+//   console.log(result);
+//   // Change code above this line
+//   return result;
+// };
+// checkForSpam("Latest technology news");
+// checkForSpam("JavaScript weekly newsletter");
+// checkForSpam('Get best sale offers now!');
+// checkForSpam("Get best sale offers now!");
+// checkForSpam("Amazing SalE, only tonight!");
+// checkForSpam("Trust me, this is not a spam message");
+// checkForSpam("Get rid of sPaM emails. Our book in on sale!");
+// checkForSpam("[SPAM] How to earn fast money?");
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   } 
+//     return "Access denied, wrong password!";
+//   // Change code above this line
+// }
+
+// console.log(checkPassword("mangohackzor"));
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   } else if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   }
+//     return "The order is accepted, our manager will contact you";
+
+//   // Change code above this line
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[fruits.length - 1];
+
+// console.log('firstElement:',firstElement);
+// console.log('secondElement', secondElement);
+// console.log('lastElement', lastElement);
